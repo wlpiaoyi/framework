b63b0ca46cf9a564e9172ac4ad5bc4242de1c377
飘逸A186 <wlpiaoyi@gmail.com>
2022-03-01 10:58:00 +0800

    支持Properties map json 的读取

--- utils/src/main/java/org/wlpiaoyi/framework/utils/data/ReaderUtils.java
+++ utils/src/main/java/org/wlpiaoyi/framework/utils/data/ReaderUtils.java
@@ -1,14 +1,32 @@
 package org.wlpiaoyi.framework.utils.data;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import lombok.NonNull;
+import org.wlpiaoyi.framework.utils.ValueUtils;
+import org.wlpiaoyi.framework.utils.gson.GsonBuilder;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Map;
+import java.util.Properties;
 
 public class ReaderUtils {
+
+
+    public static Properties loadProperties(@NonNull String path) throws IOException {
+        File file = DataUtils.loadFile(path);
+        if(file == null) throw new IOException("没有找到文件");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        Properties properties = new Properties();
+        properties.load(fileInputStream);
+        return properties;
+    }
 
     public static byte[] loadBytes(@NonNull String path) throws IOException {
         File file = DataUtils.loadFile(path);
@@ -21,6 +39,27 @@ public class ReaderUtils {
             encoding = StandardCharsets.UTF_8;
         }
         return new String(loadBytes(path), encoding);
+    }
+
+    public static JsonElement loadJson(@NonNull String path, Charset encoding) throws IOException {
+        Map map = ReaderUtils.loadMap(path, encoding);
+        if(ValueUtils.isBlank(map)) return null;
+        Gson gson = GsonBuilder.gsonDefault();
+        return gson.toJsonTree(map);
+    }
+
+    public static Map loadMap(@NonNull String path, Charset encoding) throws IOException {
+        String str = ReaderUtils.loadString(path, encoding);
+        if(ValueUtils.isBlank(str)) return null;
+        Gson gson = GsonBuilder.gsonDefault();
+        return gson.fromJson(str, Map.class);
+    }
+
+    public static <T> T loadObject(@NonNull String path, Charset encoding, Class<T> clazz) throws IOException {
+        JsonElement json = ReaderUtils.loadJson(path, encoding);
+        if(json == null) return null;
+        Gson gson = GsonBuilder.gsonDefault();
+        return gson.fromJson(json, clazz);
     }
 
 }
