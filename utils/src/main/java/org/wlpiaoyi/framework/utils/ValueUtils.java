0fc6dedb502a0913247c69532be90047cfe0e9ef
飘逸A186 <wlpiaoyi@gmail.com>
2023-12-08 19:40:45 +0800

    微调

--- utils/src/main/java/org/wlpiaoyi/framework/utils/ValueUtils.java
+++ utils/src/main/java/org/wlpiaoyi/framework/utils/ValueUtils.java
@@ -44,23 +44,23 @@ class ValueEqualsUtils extends ValueParseUtils{
     }
     private static boolean arrayEquals(Object o1, Object o2) {
         if (o1 instanceof Object[] && o2 instanceof Object[]) {
-            return Arrays.equals((Object[])((Object[])o1), (Object[])((Object[])o2));
+            return Arrays.equals((Object[])o1, (Object[])o2);
         } else if (o1 instanceof boolean[] && o2 instanceof boolean[]) {
-            return Arrays.equals((boolean[])((boolean[])o1), (boolean[])((boolean[])o2));
+            return Arrays.equals((boolean[])o1, (boolean[])o2);
         } else if (o1 instanceof byte[] && o2 instanceof byte[]) {
-            return Arrays.equals((byte[])((byte[])o1), (byte[])((byte[])o2));
+            return Arrays.equals((byte[])o1, (byte[])o2);
         } else if (o1 instanceof char[] && o2 instanceof char[]) {
-            return Arrays.equals((char[])((char[])o1), (char[])((char[])o2));
+            return Arrays.equals((char[])o1, (char[]) o2);
         } else if (o1 instanceof double[] && o2 instanceof double[]) {
-            return Arrays.equals((double[])((double[])o1), (double[])((double[])o2));
+            return Arrays.equals((double[])o1, (double[])o2);
         } else if (o1 instanceof float[] && o2 instanceof float[]) {
-            return Arrays.equals((float[])((float[])o1), (float[])((float[])o2));
+            return Arrays.equals((float[])o1, (float[])o2);
         } else if (o1 instanceof int[] && o2 instanceof int[]) {
-            return Arrays.equals((int[])((int[])o1), (int[])((int[])o2));
+            return Arrays.equals((int[])o1, (int[])o2);
         } else if (o1 instanceof long[] && o2 instanceof long[]) {
-            return Arrays.equals((long[])((long[])o1), (long[])((long[])o2));
+            return Arrays.equals((long[])o1, (long[])o2);
         } else {
-            return o1 instanceof short[] && o2 instanceof short[] ? Arrays.equals((short[])((short[])o1), (short[])((short[])o2)) : false;
+            return o1 instanceof short[] && o2 instanceof short[] ? Arrays.equals((short[])o1, (short[])o2) : false;
         }
     }
 }
@@ -84,11 +84,12 @@ class ValueParseUtils extends ValueBlankUtils{
     }
 
     public static byte[] hexToBytes(String s) {
-        int len = s.length();
+        String us = s.toUpperCase(Locale.ROOT);
+        int len = us.length();
         byte[] b = new byte[len / 2];
         for (int i = 0; i < len; i += 2) {
-            b[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character
-                    .digit(s.charAt(i + 1), 16));
+            b[i / 2] = (byte) ((Character.digit(us.charAt(i), 16) << 4) + Character
+                    .digit(us.charAt(i + 1), 16));
         }
         return b;
     }
