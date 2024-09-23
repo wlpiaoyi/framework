685df362f07e6c0b359d8b31f687b684950e32b2
飘逸A186 <wlpiaoyi@gmail.com>
2024-09-23 21:56:40 +0800

    一些优化

--- utils/src/main/java/org/wlpiaoyi/framework/utils/http/request/Request.java
+++ utils/src/main/java/org/wlpiaoyi/framework/utils/http/request/Request.java
@@ -21,7 +21,7 @@ import java.util.Map;
  * <p><b>{@code @date:}</b>2024-09-23 07:53:39</p>
  * <p><b>{@code @version:}:</b>1.0</p>
  */
-public class Request<T> implements HttpMessage {
+public class Request<T> implements HttpMessage<T> {
 
 
     /**
@@ -154,6 +154,30 @@ public class Request<T> implements HttpMessage {
 
     /**
      * <p><b>{@code @description:}</b>
+     * TODO
+     * </p>
+     * 
+     * <p><b>@param</b> <b>context</b>
+     * {@link HttpClientContext}
+     * </p>
+     * 
+     * <p><b>@param</b> <b>url</b>
+     * {@link String}
+     * </p>
+     *
+     * <p><b>{@code @date:}</b>2024/9/23 20:34</p>
+     * <p><b>{@code @return:}</b>{@link }</p>
+     * <p><b>{@code @author:}</b>wlpiaoyi</p>
+     */
+    public Request(HttpClientContext context, String url){
+        this.context = context;
+        this.url = url;
+        this.headers = HEADER_FORM_DEFAULTS();
+        this.method = Method.Get;
+    }
+
+    /**
+     * <p><b>{@code @description:}</b>
      * 执行请求
      * </p>
      *
@@ -181,7 +205,7 @@ public class Request<T> implements HttpMessage {
 
     /**
      * <p><b>{@code @description:}</b>
-     * TODO
+     * 添加Header
      * </p>
      *
      * <p><b>@param</b> <b>key</b>
@@ -196,32 +220,105 @@ public class Request<T> implements HttpMessage {
      * <p><b>{@code @return:}</b>{@link Request}</p>
      * <p><b>{@code @author:}</b>wlpiaoyi</p>
      */
-    public Request setHeader(String key, String value){
+    public Request<T> setHeader(String key, String value){
         this.headers.put(key, value);
         return this;
     }
 
-    public Request setBody(T body){
+    /**
+     * <p><b>{@code @description:}</b>
+     * 设置请求数据主体
+     * </p>
+     *
+     * <p><b>@param</b> <b>body</b>
+     * {@link T}
+     * </p>
+     *
+     * <p><b>{@code @date:}</b>2024/9/23 20:34</p>
+     * <p><b>{@code @return:}</b>{@link Request<T>}</p>
+     * <p><b>{@code @author:}</b>wlpiaoyi</p>
+     */
+    public Request<T> setBody(T body){
         this.body = body;
         return this;
     }
 
-    public Request setParams(Map<String, String> params){
+    /**
+     * <p><b>{@code @description:}</b>
+     * 设置请求参数
+     * </p>
+     *
+     * <p><b>@param</b> <b>params</b>
+     * {@link Map<String, String>}
+     * </p>
+     *
+     * <p><b>{@code @date:}</b>2024/9/23 20:35</p>
+     * <p><b>{@code @return:}</b>{@link Request<T>}</p>
+     * <p><b>{@code @author:}</b>wlpiaoyi</p>
+     */
+    public Request<T> setParams(Map<String, String> params){
         this.params = params;
         return this;
     }
 
-    public Request setParam(String name, String value){
+    /**
+     * <p><b>{@code @description:}</b>
+     * 设置请求参数
+     * </p>
+     *
+     * <p><b>@param</b> <b>name</b>
+     * {@link String}
+     * </p>
+     *
+     * <p><b>@param</b> <b>value</b>
+     * {@link String}
+     * </p>
+     *
+     * <p><b>{@code @date:}</b>2024/9/23 20:36</p>
+     * <p><b>{@code @return:}</b>{@link Request<T>}</p>
+     * <p><b>{@code @author:}</b>wlpiaoyi</p>
+     */
+    public Request<T> setParam(String name, String value){
         this.params.put(name, value);
         return this;
     }
 
-    public Request setHttpProxy(HttpHost httpProxy) {
+    /**
+     * <p><b>{@code @description:}</b>
+     * 设置代理
+     * </p>
+     *
+     * <p><b>@param</b> <b>httpProxy</b>
+     * {@link HttpHost}
+     * </p>
+     *
+     * <p><b>{@code @date:}</b>2024/9/23 20:36</p>
+     * <p><b>{@code @return:}</b>{@link Request<T>}</p>
+     * <p><b>{@code @author:}</b>wlpiaoyi</p>
+     */
+    public Request<T> setHttpProxy(HttpHost httpProxy) {
         this.httpProxy = httpProxy;
         return this;
     }
 
-    public Request setHttpProxy(final String hostname, final int port) {
+    /**
+     * <p><b>{@code @description:}</b>
+     * 设置代理
+     * </p>
+     *
+     * <p><b>@param</b> <b>hostname</b>
+     * {@link String}
+     * </p>
+     *
+     * <p><b>@param</b> <b>port</b>
+     * {@link int}
+     * </p>
+     *
+     * <p><b>{@code @date:}</b>2024/9/23 20:36</p>
+     * <p><b>{@code @return:}</b>{@link Request<T>}</p>
+     * <p><b>{@code @author:}</b>wlpiaoyi</p>
+     */
+    public Request<T> setHttpProxy(final String hostname, final int port) {
         this.httpProxy = new HttpHost(hostname, port);
         return this;
     }
